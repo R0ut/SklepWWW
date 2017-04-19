e1a4082464b531de1e14ce97adba465a6a6286fb
R0ut <kkaacczzoorr@gmail.com>
2017-04-19 20:11:09 +0200

    Dodanie KursyContext

---
+++ SklepWWW/DAL/KursyContext.cs
@@ -0,0 +1,32 @@
+﻿using SklepWWW.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace SklepWWW.DAL
+{
+    public class KursyContext : DbContext
+    {
+        public KursyContext() : base("KursyContext")
+        {
+
+        }
+
+        public DbSet<Kurs> Kursy { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+
+
+        //nadpisanie zeby nie byly dodawane s do konca nazw np. Kursys,Kategories itd.
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
