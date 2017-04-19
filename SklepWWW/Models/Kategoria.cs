237b6055ee3a5fca6f214f545f927009fba70941
R0ut <kkaacczzoorr@gmail.com>
2017-04-19 19:45:29 +0200

    dodanie klas modelu dla aplikacji

---
+++ SklepWWW/Models/Kategoria.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SklepWWW.Models
+{
+    public class Kategoria
+    {
+        public int KategoriaId { get; set; }
+        public string NazwaKategorii { get; set; }
+        public string OpisKategorii { get; set; }
+        public string NazwaPlikuIkony { get; set; }
+
+
+        public virtual ICollection<Kurs> Kursy { get; set; }
+
+    }
+}
