b65780afc22d30b7e490a128a4407fffe6119bef
R0ut <kkaacczzoorr@gmail.com>
2017-05-11 20:53:14 +0200

    Dodanie widoku kursów w wybranej kategorii

--- SklepWWW/Controllers/KursyController.cs
+++ SklepWWW/Controllers/KursyController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SklepWWW.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace SklepWWW.Controllers
 {
     public class KursyController : Controller
     {
+        private KursyContext db = new KursyContext();
         // GET: Kursy
         public ActionResult Index()
         {
@@ -16,11 +18,21 @@ namespace SklepWWW.Controllers
 
         public ActionResult Lista(string nazwaKategori)
         {
-            return View();
+            var kategorie = db.Kategorie.Include("Kursy").Where(x => x.NazwaKategorii.ToUpper() == nazwaKategori.ToUpper()).Single();
+            var kursy = kategorie.Kursy.ToList();
+            return View(kursy);
         }
         public ActionResult Szczegoly(string id)
         {
             return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult KategorieMenu()
+        {
+            var kategorie = db.Kategorie.ToList();
+
+            return PartialView("_KategorieMenu", kategorie);
         }
     }
 }
