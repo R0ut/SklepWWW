8f5e4ccdcba4071a5bb2b441841bae2c9d6c4892
R0ut <kkaacczzoorr@gmail.com>
2017-05-16 21:49:34 +0200

    mechanizm filtrowania i autopodpowiedzi ajax

--- SklepWWW/Controllers/KursyController.cs
+++ SklepWWW/Controllers/KursyController.cs
@@ -13,18 +13,27 @@ namespace SklepWWW.Controllers
         // GET: Kursy
         public ActionResult Index()
         {
-            return View(); 
+            return View();
         }
 
-        public ActionResult Lista(string nazwaKategori)
+        public ActionResult Lista(string nazwaKategori, string searchQuery = null)
         {
             var kategorie = db.Kategorie.Include("Kursy").Where(x => x.NazwaKategorii.ToUpper() == nazwaKategori.ToUpper()).Single();
-            var kursy = kategorie.Kursy.ToList();
+
+            var kursy = kategorie.Kursy.Where(x => (searchQuery == null || x.TytulKursu.ToLower().Contains(searchQuery.ToLower()) ||
+                                                x.AutorKursu.ToLower().Contains(searchQuery.ToLower())) &&
+                                                !x.Ukryty);
+
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_KursyList", kursy);
+            }
+
             return View(kursy);
         }
         public ActionResult Szczegoly(string id)
         {
-            
+
             var kurs = db.Kursy.Find(Convert.ToInt16(id));
             return View(kurs);
         }
@@ -37,5 +46,14 @@ namespace SklepWWW.Controllers
 
             return PartialView("_KategorieMenu", kategorie);
         }
+        public ActionResult KursyPodpowiedzi(string term)
+        {
+            var kursy = db.Kursy.Where(x => !x.Ukryty && x.TytulKursu.ToLower().Contains(term.ToLower()))
+                        .Take(5).Select(x => new { label = x.TytulKursu });
+
+
+            return Json(kursy, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
