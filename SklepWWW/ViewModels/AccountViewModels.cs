baec1651fedd2ffbe64ca98300e88233293c3f7a
R0ut <kkaacczzoorr@gmail.com>
2017-05-22 22:58:51 +0200

    DOdanie widoków rejestracji i logowania

---
+++ SklepWWW/ViewModels/AccountViewModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SklepWWW.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = " Musisz wpisać e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = " Musisz wprowadzić hasło")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} musi mieć coanmniej {2} znaków", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+       
+        [StringLength(100, ErrorMessage = "{0} musi mieć coanmniej {2} znaków", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwirdź hasło")]
+        [Compare("Password", ErrorMessage = "Hasła nie pasują do siebie")]
+        public string ConfirmPassword { get; set; }
+    }
+}
